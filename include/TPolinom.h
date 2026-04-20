525fc9b17d20f0e8f0a915cdb42b591f6dcb049d
Chehovsky Grigory <cehovskijgrisa@gmail.com>
2026-04-20 21:26:56 +0300

    ..

---
+++ include/TPolinom.h
@@ -0,0 +1,255 @@
+#include "TLists.h"
+#include "TMonom.h"
+#include <iostream>
+#include <regex>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace regex_constants;
+
+vector<string> split(const string &str, char delimiter) {
+  vector<string> tokens;
+  stringstream ss(str);
+  string token;
+  while (getline(ss, token, delimiter)) {
+    tokens.push_back(token);
+  }
+  return tokens;
+}
+
+class TPolinom {
+private:
+  TSinglyList<TMonom> list;
+
+public:
+  TPolinom() { list.PushFront(TMonom(0, 0)); }
+  TPolinom(double num) { list.PushFront(TMonom(num, 0)); }
+  TPolinom(string st) {
+    if (st.size() == 0) {
+      TPolinom r;
+      *this = r;
+
+    } else {
+      vector<string> res = split(st, '+');
+      regex pattern(R"(^[-]?[0-9.]*x[0-9]y[0-9]z[0-9]$)");
+      for (size_t i = 0; i < res.size(); i++) {
+        if (!regex_match(res[i], pattern)) {
+          throw invalid_argument("Invalid input" + res[i]);
+        }
+      }
+      for (size_t i = 0; i < res.size(); i++) {
+        size_t tmp = res[i].find('x');
+        string number;
+        int itmp = 0;
+        while (itmp != tmp) {
+          number += res[i][itmp];
+          itmp++;
+        }
+        string number_for_degree;
+        number_for_degree += res[i][tmp + 1];
+        number_for_degree += res[i][tmp + 3];
+        number_for_degree += res[i][tmp + 5];
+        list.PushFront(TMonom(stod(number), stoi(number_for_degree)));
+      }
+      this->sort();
+    }
+  }
+
+  TPolinom(const TPolinom &p) { list = p.list; }
+
+  void sort() {
+    if (!list.GetFirst() || !list.GetFirst()->pNext)
+      return;
+    bool swapped;
+    do {
+      swapped = false;
+      typename TSinglyList<TMonom>::TNode *current = list.GetFirst();
+      while (current->pNext != nullptr) {
+        if (current->val < current->pNext->val) {
+          TMonom temp = current->val;
+          current->val = current->pNext->val;
+          current->pNext->val = temp;
+          swapped = true;
+        }
+        current = current->pNext;
+      }
+    } while (swapped);
+  }
+
+  TPolinom operator+(const TPolinom &p) {
+    typename TSinglyList<TMonom>::TNode *Node_1 = list.GetFirst();
+    typename TSinglyList<TMonom>::TNode *Node_2 = p.list.GetFirst();
+    TPolinom res;
+    while (Node_1 != nullptr && Node_2 != nullptr) {
+      if (Node_1->val == Node_2->val) {
+        double newCoeff = Node_1->val.Ratio() + Node_2->val.Ratio();
+        if (newCoeff != 0) {
+          res.list.PushFront(TMonom(newCoeff, Node_1->val.Degree()));
+        }
+        Node_1 = Node_1->pNext;
+        Node_2 = Node_2->pNext;
+      } else if (Node_1->val > Node_2->val) {
+        res.list.PushFront(TMonom(Node_1->val.Ratio(), Node_1->val.Degree()));
+        Node_1 = Node_1->pNext;
+      } else {
+        res.list.PushFront(TMonom(Node_2->val.Ratio(), Node_2->val.Degree()));
+        Node_2 = Node_2->pNext;
+      }
+    }
+    while (Node_1 != nullptr) {
+      res.list.PushFront(TMonom(Node_1->val.Ratio(), Node_1->val.Degree()));
+      Node_1 = Node_1->pNext;
+    }
+    while (Node_2 != nullptr) {
+      res.list.PushFront(TMonom(Node_2->val.Ratio(), Node_2->val.Degree()));
+      Node_2 = Node_2->pNext;
+    }
+    // res.sort();
+    return res;
+  }
+
+  TPolinom operator*(const double value) const {
+    TPolinom res = *this;
+    typename TSinglyList<TMonom>::TNode *Node_1 = res.list.GetFirst();
+    while (Node_1 != nullptr) {
+      Node_1->val = TMonom(Node_1->val.Ratio() * value, Node_1->val.Degree());
+      Node_1 = Node_1->pNext;
+    }
+    return res;
+  }
+
+  TPolinom &operator=(const TPolinom &t) {
+    if (this != &t) {
+      list = t.list;
+    }
+    return *this;
+  }
+
+  TPolinom operator*(const TMonom &m) const {
+    TPolinom res = *this;
+    typename TSinglyList<TMonom>::TNode *Node_2 = res.list.GetFirst();
+    while (Node_2 != nullptr) {
+      Node_2->val = TMonom((Node_2->val) * m);
+      Node_2 = Node_2->pNext;
+    }
+    return res;
+  }
+
+  TPolinom &operator+=(const TMonom &m) {
+    typename TSinglyList<TMonom>::TNode *Node_1 = list.GetFirst();
+    bool found = false;
+    while (Node_1 != nullptr && !found) {
+      if (Node_1->val.Degree() == m.Degree()) {
+        double newCoeff = Node_1->val.Ratio() + m.Ratio();
+        Node_1->val = TMonom(newCoeff, m.Degree());
+        found = true;
+      }
+      Node_1 = Node_1->pNext;
+    }
+    if (!found && m.Ratio() != 0) {
+      list.PushFront(m);
+      sort();
+    }
+    return *this;
+  }
+
+  TPolinom &operator+=(const TPolinom &p) {
+    *this = *this + p;
+    return *this;
+  }
+
+  TPolinom operator*(const TPolinom &p) {
+    TPolinom res;
+    typename TSinglyList<TMonom>::TNode *Node_1 = list.GetFirst();
+    while (Node_1 != nullptr) {
+      typename TSinglyList<TMonom>::TNode *Node_2 = p.list.GetFirst();
+      while (Node_2 != nullptr) {
+        TMonom product = Node_1->val * Node_2->val;
+        res += product;
+        Node_2 = Node_2->pNext;
+      }
+      Node_1 = Node_1->pNext;
+    }
+    return res;
+  }
+
+  bool operator==(TPolinom &t) {
+    this->sort();
+    t.sort();
+    return list == t.list;
+  }
+
+  TPolinom operator-(const TPolinom &t) { return *this + t * (-1); }
+
+  friend ostream &operator<<(ostream &os, const TPolinom &pol) {
+    typename TSinglyList<TMonom>::TNode *Node_1 = pol.list.GetFirst();
+    size_t counter = 0;
+    while (Node_1 != nullptr) {
+      if(Node_1->val.Degree()==0 && Node_1->val.Ratio()==0){
+        Node_1=Node_1->pNext;
+      }else{
+      if (counter == 0) {
+        if (Node_1->val.Degree() == 0) {
+          os << Node_1->val.Ratio();
+        } else {
+          os << Node_1->val;
+        }
+      } else {
+        if (Node_1->val.Ratio() < 0) {
+          if (Node_1->val.Degree() == 0) {
+            os << Node_1->val.Ratio();
+          } else {
+            os << Node_1->val;
+          }
+
+        } else if (Node_1->val.Ratio() > 0) {
+          if (Node_1->val.Degree() == 0) {
+            os << '+' << Node_1->val.Ratio();
+          } else {
+            os << '+' << Node_1->val;
+          }
+        }
+      }
+      counter++;
+      Node_1 = Node_1->pNext;
+    }}
+    if (counter == 0) {
+      os << "0";
+    }
+    return os;
+  }
+
+  void stick() {
+    typename TSinglyList<TMonom>::TNode *Node_1 = list.GetFirst();
+    while (Node_1 != nullptr && Node_1->pNext != nullptr) {
+      if (Node_1->val.Degree() == Node_1->pNext->val.Degree()) {
+        Node_1->val = TMonom(Node_1->val.Ratio() + Node_1->pNext->val.Ratio(),
+                             Node_1->val.Degree());
+        delete Node_1->pNext;
+        Node_1->pNext = Node_1->pNext->pNext;
+      }
+      Node_1 = Node_1->pNext;
+    }
+  }
+
+  double Calculate_At(const double &_x, const double &_y, const double &_z) {
+    TPolinom tmp(*this);
+    
+    typename TSinglyList<TMonom>::TNode *Node_1 = tmp.list.GetFirst();
+    bool flag_is_calculate_over = false;
+    double result = 0;
+    while (!flag_is_calculate_over) {
+      if (Node_1->pNext == nullptr) {
+        flag_is_calculate_over = true;
+      }
+      result += Node_1->val.Ratio() * pow(_x, Node_1->val.Degree() / 100) *
+                pow(_y, Node_1->val.Degree() % 100 / 10) *
+                pow(_z, Node_1->val.Degree() % 10);
+      Node_1 = Node_1->pNext;
+    }
+    return result;
+  }
+};
---
+++ include/map.h
@@ -0,0 +1,254 @@
+#ifndef __MAP_H__
+#define __MAP_H__
+
+#include <vector>
+#include <stdexcept>
+#include "TLists.h"
+
+using namespace std;
+
+template <typename TKey, typename TValue>
+class Map {
+public:
+    
+    struct Pair {
+        TKey key;
+        TValue value;
+        
+        Pair() = default;  
+        Pair(const TKey& k, const TValue& v) : key(k), value(v) {}
+        
+        bool operator<(const Pair& other) const { return key < other.key; }
+        bool operator>(const Pair& other) const { return key > other.key; }
+        bool operator==(const Pair& other) const { return key == other.key; }
+        
+        
+        bool operator==(const TKey& otherKey) const { return key == otherKey; }
+    };
+    
+    
+    virtual ~Map() = default;
+    
+    
+    virtual TValue* Find(const TKey& key) = 0;
+    virtual void Insert(const TKey& key, const TValue& value) = 0;
+    virtual void Delete(const TKey& key) = 0;
+    virtual int count() const = 0;
+    virtual bool isempty() const { return Count() == 0; }
+    virtual TValue& operator[](const TKey& key) = 0;
+    virtual vector<TKey> keys() const = 0;
+    virtual vector<TValue> values() const = 0;
+    virtual bool containsKey(const TKey& key) const {
+        return Find(key) != nullptr;
+    }
+};
+
+template <typename TKey, typename TValue>
+class OrderedMap : public Map<TKey, TValue> {
+private:
+    using typename Map<TKey, TValue>::Pair;
+    vector<Pair> data;
+    
+    int BinarySearchWithPos(const TKey& key) const {
+        int left = 0;
+        int right = data.size() - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (data[mid].key == key) {
+                return mid;  
+            }
+            if (data[mid].key < key) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -left - 1;
+    }
+    
+public:
+    OrderedMap() = default;
+    ~OrderedMap() = default;
+    
+    TValue* Find(const TKey& key) override {
+        int result = BinarySearchWithPos(key);
+        if (result >= 0) {
+            return &data[result].value;
+        }
+        return nullptr;
+    }
+    void Insert(const TKey& key, const TValue& value) override {
+    int result = BinarySearchWithPos(key);
+    
+    if (result >= 0) {
+        data[result].value = value;
+        return;
+    }
+    size_t insertPos = -result - 1;  
+    data.insert(data.begin() + insertPos, Pair(key, value));
+}
+    
+    void Delete(const TKey& key) override {
+        int result = BinarySearchWithPos(key);
+        
+        if (result < 0) {
+            throw invalid_argument("Key not found");
+        }
+        
+        data.erase(data.begin() + result);
+    }
+    
+    int Count() const override {
+        return data.size();
+    }
+    
+    TValue& operator[](const TKey& key) override {
+        int result = BinarySearchWithPos(key);
+        
+        if (result >= 0) {
+            return data[result].value;
+        }
+        size_t insertPos = -result - 1;
+        data.insert(data.begin() + insertPos, Pair(key, TValue()));
+        return data[insertPos].value;
+    }
+    vector<TKey> Keys() const override {
+    vector<TKey> result;
+    for (const auto& pair : data) {
+        result.push_back(pair.key);
+    }
+    return result;
+}
+    
+    vector<TValue> Values() const override {
+        vector<TValue> result;
+        for (const auto& pair : data) {
+            result.push_back(pair.value);
+        }
+        return result;
+    }
+    
+    void Clear() override {
+        data.clear();
+    }
+    
+    void Print() const {
+        if (data.empty()) {
+            cout << "{}" << endl;
+            return;
+        }
+        
+        cout << "{" << endl;
+        for (size_t i = 0; i < data.size(); i++) {
+            cout << "  " << data[i].key << " : " << data[i].value;
+            if (i != data.size() - 1) {
+                cout << ",";
+            }
+            cout << endl;
+        }
+        cout << "}" << endl;
+    }
+};
+
+
+template <typename TKey, typename TValue>
+class UnorderedMap : public Map<TKey, TValue> {
+private:
+    using typename Map<TKey, TValue>::Pair;  
+    TSinglyList<Pair> list;  
+    
+public:
+    UnorderedMap() = default;
+    ~UnorderedMap() = default;
+
+    TValue* Find(const TKey& key) override {
+        for (int i = 0; i < list.size(); i++) {
+            if (list[i] == key) { 
+                return &list[i].value;
+            }
+        }
+        return nullptr;
+    }
+    
+    
+    void Insert(const TKey& key, const TValue& value) override {
+    for (int i = 0; i < list.size(); i++) {
+        if (list[i] == key) {
+            list[i].value = value;  
+            return;
+        }
+    }
+    list.PushFront(Pair(key, value));
+}
+    void Delete(const TKey& key) override {
+        if (list.isEmpty()) {
+            throw invalid_argument("Key not found");
+        }
+        if (list.GetFirst()->val == key) {  
+            list.PopFront();
+            return;
+        }
+        for (int i = 1; i < list.size(); i++) {
+            if (list[i] == key) { 
+                list.EraseAfter(i - 1);
+                return;
+            }
+        }
+        
+        throw invalid_argument("Key not found");
+    }
+    int Count() const override {
+        return list.size();
+    }
+    TValue& operator[](const TKey& key) override {
+        TValue* val = Find(key);
+        if (val != nullptr) {
+            return *val;
+        }
+        list.PushFront(Pair(key, TValue()));
+        return list.GetFirst()->val.value;
+    }
+    vector<TKey> Keys() const override {
+        vector<TKey> result;
+        result.reserve(list.size());
+        
+        for (int i = 0; i < list.size(); i++) {
+            result.push_back(list[i].key);
+        }
+        return result;
+    }
+    vector<TValue> Values() const override {
+        vector<TValue> result;
+        result.reserve(list.size());
+        
+        for (int i = 0; i < list.size(); i++) {
+            result.push_back(list[i].value);
+        }
+        return result;
+    }
+    void Clear() override {
+        while (!list.isEmpty()) {
+            list.PopFront();
+        }
+    }
+    void Print() const {
+        if (list.isEmpty()) {
+            cout << "{}" << endl;
+            return;
+        }
+        
+        cout << "{" << endl;
+        for (int i = 0; i < list.size(); i++) {
+            cout << "  " << list[i].key << " : " << list[i].value;
+            if (i != list.size() - 1) {
+                cout << ",";
+            }
+            cout << endl;
+        }
+        cout << "}" << endl;
+    }
+};
+
+#endif
